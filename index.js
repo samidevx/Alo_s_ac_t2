23b7fe3c268445d10e340cfa8c30870e06120945
islambou9427 <islem.bouagada@gmail.com>
2022-04-02 15:14:05 +0100

    install parse-int

--- index.js
+++ index.js
@@ -1,39 +1,72 @@
 const express = require('express')
 const app = express()
-const parkings = require('./parkings.json')
+const db = require('./db.json')
+var parseInt = require('parse-int');
 
 app.use(express.json())
+app.use(logErrors);
+app.use(dbErrorHandler);
+app.use(errorHandler);
 
-app.get('/parkings', (req,res) => {
-    res.status(200).json(parkings)
+//Find all db
+app.get('/db', (req,res) => {
+    res.status(200).json(db)
 })
 
-app.get('/parkings/:id', (req,res) => {
+
+ 
+//Find db details
+app.get('/db/:id', (req,res) => {
+    
     const id = parseInt(req.params.id)
-    const parking = parkings.find(parking => parking.id === id)
-    res.status(200).json(parking)
+    const db = db.find(db => db.id === id)
+    res.status(200).json(db)
 })
 
-app.post('/parkings', (req,res) => {
-    parkings.push(req.body)
-    res.status(200).json(parkings)
+//creates a db
+app.post('/db', (req,res) => {
+    db.push(req.body)
+    res.status(200).json(db)
 })
-app.put('/parkings/:id', (req,res) => {
+
+//updates a db
+app.put('/db/:id', (req,res) => {
     const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parking.name =req.body.name,
-    parking.city =req.body.city,
-    parking.type =req.body.type,
-    res.status(200).json(parking)
+    let db = db.find(db => db.id === id)
+    db.name =req.body.name,
+    db.sku=req.body.sku,
+    db.salePrice =req.body.salePrice,
+    res.status(200).json(db)
 })
 
-app.delete('/parkings/:id', (req,res) => {
+//Deletes a db
+app.delete('/db/:id', (req,res) => {
     const id = parseInt(req.params.id)
-    let parking = parkings.find(parking => parking.id === id)
-    parkings.splice(parkings.indexOf(parking),1)
-    res.status(200).json(parkings)
+    let db = db.find(db => db._id === id)
+    db.splice(db.indexOf(db),1)
+    res.status(200).json(db)
 })
 
-app.listen(8080, () => {
+
+//Development-only error handler
+function logErrors(err, req, res, next) {
+    console.error(err.stack);
+    next(err);
+  }
+  function dbErrorHandler(err, req, res, next) {
+    if (req.xhr) {
+      res.status(500).send({ error: 'Something failed!' });
+    } else {
+      next(err);
+    }
+  }
+  function errorHandler(err, req, res, next) {
+    res.status(500);
+    res.render('error', { error: err });
+  }
+
+
+
+app.listen(3000, () => {
     console.log("Serveur à l'écoute")
 })
